430d5ea1f7a4eeddd12242642398401f7d7de96a
Jesse Smidt <jsmidt@f0r4s12.codam.nl>
2026-05-06 15:36:52 +0200

    first commit

--- src/augmenter.py
+++
@@ -1,6 +0,0 @@
-# Augmenting: Once the AI has retrieved the information, it can combine it with
-# what it already “knows.” However, in most practical applications, we try to rely as
-# much as possible on the retrieved data rather than the model’s internal knowledge
-# — since mixing both may lead to outdated or hallucinatory answers. Starting from
-# the retrieved results, you can clean and filter them to remove irrelevant snippets
-# (to avoid potential noise), insert them into the context window.
--- src/generator.py
+++ src/generator.py
@@ -1,3 +1,10 @@
+# Augmenting: Once the AI has retrieved the information, it can combine it with
+# what it already “knows.” However, in most practical applications, we try to rely as
+# much as possible on the retrieved data rather than the model’s internal knowledge
+# — since mixing both may lead to outdated or hallucinatory answers. Starting from
+# the retrieved results, you can clean and filter them to remove irrelevant snippets
+# (to avoid potential noise), insert them into the context window.
+
 # Generating: Now that you have retrieved the information and augmented it,
 # the AI can finally generate an answer! Whether it’s writing text, explaining a
 # concept, or producing code snippets, this is the visible outcome of RAG. To do
